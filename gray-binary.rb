11314816068c27121d17a36f51f623317b20f183
hachiya <hachiya@sein.mse.kyutech.ac.jp>
2015-07-13 11:30:11 +0000

    master

--- gray-binary.rb
+++ gray-binary.rb
@@ -1,1 +1,14 @@
-gray = gets.to_i.to_s(2).split
+puts "Input:"
+gray = gets.to_i.to_s(2).split("")
+if gray.join.to_i(2) > 1023 then
+puts "Over maximum bit width"
+exit
+end
+binary = Array.new
+j = 0
+gray.each do |i|
+j = j^i.to_i
+binary << j.to_s
+end
+puts "Output:"
+puts "Binary code #{binary.join.to_i(2)}"
